7a995a321e6b665d2be4ee614db6b4704b2e02f8
m01x <fortiz.informatica@gmail.com>
2024-08-29 22:25:09 -0400

    feat(test/jest): Se agrega la configuracion de Jest y dependencias -D, de whatwg-fetch, para trabajos con node pre version 18

--- src/base-pruebas/11-async-await.js
+++ src/base-pruebas/11-async-await.js
@@ -1,32 +1,18 @@
+export const getImagen = async () => {
+  try {
+    const apiKey = 'txL7WZKNogArWRR2LRA8HJ2ZXXgBWHo5';
+    const resp = await fetch(
+      `http://api.giphy.com/v1/gifs/random?api_key=${apiKey}`
+    );
+    const { data } = await resp.json();
 
+    const { url } = data.images.original;
+    return url;
+  } catch (error) {
+    // manejo del error
+    console.error(error);
+    return 'No se encontro la imagen';
+  }
+};
 
-// const getImagenPromesa = () => new Promise( resolve => resolve('https://ajskdhaskjdhajs.com') )
-// getImagenPromesa().then( console.log );
-
-const getImagen = async() => {
-
-    try {
-
-        const apiKey = 'C1khQe3Z7R1W2lfTO9myKeuShdqFYSGC';
-        const resp   = await fetch(`http://api.giphy.com/v1/gifs/random?api_key=${ apiKey }`);
-        const { data } = await resp.json(); 
-
-        const { url } = data.images.original;
-
-        const img = document.createElement('img');
-        img.src = url;
-        document.body.append( img );
-
-    } catch (error) {
-        // manejo del error
-        console.error(error)
-    }
-    
-    
-    
-}
-
- getImagen();
-
-
-
+getImagen();
